c4c02b75422fb19686f167ad6482e0d1ebecaa3a
Elton Santos <eltonaxl@hotmail.com>
2017-01-27 19:20:12 -0300

    First commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem "httparty"
+
+# Specify your gem's dependencies in groovehq.gemspec
+gemspec
---
+++ spec/animatedgifme_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Animatedgifme do
+  it "Has a version number" do
+    expect(Animatedgifme::VERSION).not_to be nil
+  end
+
+  it "Does something useful" do
+    expect(true).to eq(true)
+  end
+end
